ce0466d0a54d5cbf4bf135d4f720227f658b068d
Shannon E. Albeke <salbeke@uwyo.edu>
2017-08-14 13:18:17 -0600

    Edits after 1st half

--- Main.R
+++ Main.R
@@ -31,47 +31,43 @@ tm_shape(mass_counties) +
   tm_text("NAME")
 #Same map, but filled with median income
 tm_shape(mass_counties) +
-  tm_polygons("B19013_001E") +
+  tm_polygons(col = "B19013_001E") +
   tm_text("NAME")
 #Not an informative representation of median income
 tm_shape(mass_counties) +
-  tm_polygons("beige") +
-  tm_bubbles("B19013_001E", scale = 5, alpha = 0.6,
+  tm_polygons(col = "beige") +
+  tm_bubbles(size = "B19013_001E", scale = 5, alpha = 0.6,
              col = "red", title.size = "Median Income")
 
+#Census done every 10 years, long form pre-2000, now is the short form
+#ACS completed annually, same as long form, 1/100 people, 1 year survey, 5 year average. 1 year data nust yse geographies > 60K
+#so that accidental disclosure doesn't happen
 # Getting ACS Data
-
-commute_vars = paste("B08301", str_pad(1:21, 3, "left", "0"), sep = "_")
-mass_commute = get_acs(
-  geography = "county",
-  variables = commute_vars,
-  endyear = 2015,
-  output = "tidy",
-  state = "MA",
-  geometry = TRUE
-)
-mass_commute$NAME = str_replace(
-  mass_commute$NAME, " County, Massachusetts", ""
-)
+#Build a list of variables by pasting come data together
+commute_vars <- paste("B08301", str_pad(string = 1:21, width = 3, side = "left", pad = "0"), sep = "_")
+mass_commute <- get_acs(geography = "county", variables = commute_vars, endyear = 2015, output = "tidy", state = "MA", geometry = TRUE)
+mass_commute$NAME <- str_replace(mass_commute$NAME, " County, Massachusetts", "")
 
 # Extract drivers (2), transit riders (10), and bikers (18)
-commute_filter = c("B08301_002", "B08301_010", "B08301_018")
+commute_filter <- c("B08301_002", "B08301_010", "B08301_018")
 # More verbose, but can makes longer list easier:
-commute_filter = paste(
-  "B08301", str_pad(c(2, 10, 18), 3, "left", "0"), sep = "_"
-)
+commute_filter <- paste("B08301", str_pad(c(2, 10, 18), 3, "left", "0"), sep = "_")
+
+#dplyr piping method, filter rows of data, plot it, has to be named variable because that is a column in mass_commute
 mass_commute %>%
   filter(variable %in% commute_filter) %>%
   ggplot(aes(x = NAME, y = estimate, fill = variable)) +
   geom_col() +
   xlab("County") + ylab("count") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+#100% fill
 mass_commute %>%
   filter(variable %in% commute_filter) %>%
   ggplot(aes(x = NAME, y = estimate, fill = variable)) +
   geom_col(position = "dodge") +
   xlab("County") + ylab("count") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
 mass_commute %>%
   filter(variable %in% commute_filter) %>%
   ggplot(aes(x = NAME, y = estimate, fill = variable)) +
@@ -85,17 +81,10 @@ tm_shape(mass_commute) +
   tm_text("NAME")
 
 # Get county geometries again. Include MHI.
-mass_counties = get_acs(
-  geography = "county",
-  variables = c("B19013_001", commute_vars),
-  endyear = 2015,
-  output = "wide",
-  state = "MA",
-  geometry = TRUE
-)
-mass_counties$NAME = str_replace(
-  mass_counties$NAME, " County, Massachusetts", ""
-)
+mass_counties <- get_acs(geography = "county", variables = c("B19013_001", commute_vars), endyear = 2015, output = "wide", 
+                         state = "MA", geometry = TRUE)
+
+mass_counties$NAME <- str_replace(mass_counties$NAME, " County, Massachusetts", "")
 
 # mass_counties_data = mutate(
 #   mass_counties,
@@ -104,16 +93,13 @@ mass_counties$NAME = str_replace(
 #   pct_bike = 100 * B08301_016E / B08301_001E
 # )
 
-mass_counties$pct_drive = 
-  100 * mass_counties$B08301_002E / mass_counties$B08301_001E
-mass_counties$pct_transit = 
-  100 * mass_counties$B08301_010E / mass_counties$B08301_001E
-mass_counties$pct_bike = 
-  100 * mass_counties$B08301_016E / mass_counties$B08301_001E
+#
+mass_counties$pct_drive <- 100 * (mass_counties$B08301_002E / mass_counties$B08301_001E)
+mass_counties$pct_transit <- 100 * (mass_counties$B08301_010E / mass_counties$B08301_001E)
+mass_counties$pct_bike <- 100 * (mass_counties$B08301_016E / mass_counties$B08301_001E)
 
 tm_shape(mass_counties) +
   tm_polygons(
-    c("pct_drive", "pct_transit"),
-    title = c("Percent Car Commuters", "Percent Transit Commuters")
+    c("pct_drive", "pct_transit"), title = c("Percent Car Commuters", "Percent Transit Commuters")
   )
 
